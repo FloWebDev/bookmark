9d5fcf7388aaf9f89ff71d4700a7d92873eee493
FloWebDev <mathevon.florian@gmail.com>
2021-04-30 23:31:58 +0200

    add fontawesome and custom login page

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -19,10 +19,17 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         $user = new User();
-        $user->setUsername('test');
-        $user->setPassword($this->passwordEncoder->encodePassword($user, 'test'));
+        $user->setUsername('admin');
+        $user->setPassword($this->passwordEncoder->encodePassword($user, 'admin'));
+        $user->setRole('ROLE_ADMIN');
+        $user->setEmail('admin@admin.io');
+        $manager->persist($user);
+
+        $user = new User();
+        $user->setUsername('user');
+        $user->setPassword($this->passwordEncoder->encodePassword($user, 'user'));
         $user->setRole('ROLE_USER');
-        $user->setEmail('test@test.io');
+        $user->setEmail('user@user.io');
         $manager->persist($user);
 
         $manager->flush();
---
+++ src/Entity/Item.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ItemRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ItemRepository::class)
+ */
+class Item
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=512)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=2048)
+     */
+    private $url;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $z;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $note;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getUrl(): ?string
+    {
+        return $this->url;
+    }
+
+    public function setUrl(string $url): self
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    public function getZ(): ?int
+    {
+        return $this->z;
+    }
+
+    public function setZ(int $z): self
+    {
+        $this->z = $z;
+
+        return $this;
+    }
+
+    public function getNote(): ?string
+    {
+        return $this->note;
+    }
+
+    public function setNote(?string $note): self
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+}
