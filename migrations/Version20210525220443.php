2c053b47036d26df5c53280a5a136a9ef689296a
FloWebDev <mathevon.florian@gmail.com>
2021-05-26 00:07:00 +0200

    nouvelle migration

--- migrations/Version20210525220443.php
+++ migrations/Version20210525220443.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210430220356 extends AbstractMigration
+final class Version20210525220443 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,10 +20,11 @@ final class Version20210430220356 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE "app_user" (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username VARCHAR(180) NOT NULL, password VARCHAR(255) NOT NULL, email VARCHAR(256) NOT NULL, role VARCHAR(32) NOT NULL, created_at DATETIME NOT NULL)');
+        $this->addSql('CREATE TABLE "app_user" (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username VARCHAR(32) NOT NULL, password VARCHAR(255) NOT NULL, email VARCHAR(256) NOT NULL, slug VARCHAR(32) NOT NULL, role VARCHAR(32) NOT NULL, created_at DATETIME NOT NULL, connected_at DATETIME DEFAULT NULL, wallpaper VARCHAR(32) DEFAULT NULL)');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_88BDF3E9F85E0677 ON "app_user" (username)');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_88BDF3E9E7927C74 ON "app_user" (email)');
-        $this->addSql('CREATE TABLE item (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, listing_id INTEGER DEFAULT NULL, title VARCHAR(512) NOT NULL, url VARCHAR(2048) NOT NULL, z INTEGER NOT NULL, note CLOB DEFAULT NULL, created_at DATETIME NOT NULL)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_88BDF3E9989D9B62 ON "app_user" (slug)');
+        $this->addSql('CREATE TABLE item (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, listing_id INTEGER DEFAULT NULL, title VARCHAR(512) NOT NULL, url VARCHAR(2048) DEFAULT NULL, z INTEGER NOT NULL, note CLOB DEFAULT NULL, created_at DATETIME NOT NULL)');
         $this->addSql('CREATE INDEX IDX_1F1B251ED4619D1A ON item (listing_id)');
         $this->addSql('CREATE TABLE listing (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, page_id INTEGER DEFAULT NULL, title VARCHAR(64) NOT NULL, z INTEGER NOT NULL, created_at DATETIME NOT NULL)');
         $this->addSql('CREATE INDEX IDX_CB0048D4C4663E4 ON listing (page_id)');
