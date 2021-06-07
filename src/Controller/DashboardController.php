43238bcb21e15da3c22363810d74ac2270beb2ce
FloWebDev <mathevon.florian@gmail.com>
2021-06-07 22:24:36 +0200

    mise en cache galerie wallpapers

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -6,16 +6,20 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Contracts\Cache\CacheInterface;
 
 class DashboardController extends AbstractController
 {
     #[Route('/dashboard', name: 'dashboard')]
-    public function index(): Response
+    public function index(CacheInterface $cache): Response
     {
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_REMEMBERED');
 
         return $this->render('dashboard/index.html.twig', [
-            'pages'     => $this->getUser()->getPages()
+            'pages'            => $this->getUser()->getPages(),
+            'gallery_template' => $cache->get('gallery-template', function() {
+                return $this->renderView('_partials/_wallpapers_gallery.html.twig');
+            })
         ]);
     }
 
